cefac4d4693404710ee4c6456943911e82845571
bakuryu90 <tranvantam.bk@gmail.com>
2018-06-24 22:22:25 +0700

    24-06-2018

---
+++ app/src/main/java/com/example/duongthuhien/kltn/ChuCai/DSBHActivity.java
@@ -0,0 +1,93 @@
+package com.example.duongthuhien.kltn.ChuCai;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.duongthuhien.kltn.Adapter.BaiHocH_adapter;
+import com.example.duongthuhien.kltn.Model.BaiHocH;
+import com.example.duongthuhien.kltn.R;
+
+import java.util.ArrayList;
+
+public class DSBHActivity extends AppCompatActivity  {
+    ListView lv_BaihocHiragana;
+    ArrayList<BaiHocH> dsBaiHoc;
+    BaiHocH_adapter baiHocH_adapter;
+    int trangthaibtn;
+    TextView tv_BCCV;
+    TextView tv_BCCJ;
+    ImageView img_BCC;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hiragana);
+
+        Intent intent=getIntent();
+        trangthaibtn=intent.getIntExtra("trangthaibtn",-1);
+        Log.d("hiendt","DSBHActivity trangthaibtn " + trangthaibtn);
+
+        addControl();
+
+
+    }
+
+    private void addControl() {
+        img_BCC=findViewById(R.id.img_BCC);
+        tv_BCCJ=findViewById(R.id.tv_BCCJ);
+        tv_BCCV=findViewById(R.id.tv_BCCV);
+        lv_BaihocHiragana = findViewById(R.id.lv_BaihocHiragana);
+        dsBaiHoc = new ArrayList<>();
+        if (trangthaibtn==2){
+            img_BCC.setImageResource(R.drawable.iconhiragana);
+            tv_BCCV.setText("Hiragana");
+            tv_BCCJ.setText("ひらがな");
+
+            String[] stringArray = getResources().getStringArray(R.array.arrBaihoc_Hiragana);
+
+            for (int i = 0; i <= 7; i++) {
+
+                dsBaiHoc.add(new BaiHocH(stringArray[i]));
+            }
+
+        }
+        else if (trangthaibtn==1){
+            img_BCC.setImageResource(R.drawable.katakana);
+            tv_BCCV.setText("Katakana");
+            tv_BCCJ.setText("カタカナ");
+            String[] stringArray = getResources().getStringArray(R.array.arrBaihoc_Katakana);
+
+            for (int i = 0; i <= 7; i++) {
+
+                dsBaiHoc.add(new BaiHocH(stringArray[i]));
+            }
+
+        }
+
+        baiHocH_adapter = new BaiHocH_adapter(DSBHActivity.this, R.layout.layout_listview, dsBaiHoc);
+
+        lv_BaihocHiragana.setAdapter(baiHocH_adapter);
+        lv_BaihocHiragana.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent=new Intent(DSBHActivity.this, HBCCActivity.class);
+                intent.putExtra("positionListview",position);
+                intent.putExtra("A",trangthaibtn);
+                startActivityForResult(intent,0);
+            }
+        });
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+}
